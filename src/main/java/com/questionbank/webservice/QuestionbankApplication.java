06c6694078f5070d39034c4eab697387f7efed64
keunyop <kylee1112@hotmail.com>
2019-01-17 00:10:51 +0900

    first commit

---
+++ src/main/java/com/questionbank/webservice/QuestionbankApplication.java
@@ -0,0 +1,14 @@
+package com.questionbank.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionbankApplication.class, args);
+	}
+
+}
+
