d4afc4f96d205505dff320325cbf8f79fc17d76e
keunyop <kylee1112@hotmail.com>
2019-02-01 15:00:48 +0900

    button control

--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -1,8 +1,18 @@
 var main = {
     init : function () {
         var _this = this;
-        $('#btn-take-the-quiz').on('click', function () {
+        var _btn_take_the_quiz = $('#btn-take-the-quiz');
+
+        _btn_take_the_quiz.on('click', function () {
             _this.openQuiz();
+        });
+
+        // Radio button change event
+        $('input:radio[name="quizRadio"]').on('change', function() {
+            if (_btn_take_the_quiz.prop("disabled")) {
+                _btn_take_the_quiz.removeClass('btn-secondary').addClass('btn-primary');
+                _btn_take_the_quiz.prop("disabled", false);          
+            }           
         });
     },
     openQuiz : function () {
