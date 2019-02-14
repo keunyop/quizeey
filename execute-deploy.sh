446dce4ca6cd9bb3b90bf67573688f61b1aadc9f
keunyop <kylee1112@hotmail.com>
2019-02-15 07:13:07 +0900

    fix

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/home/ec2-user/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+# /home/ec2-user/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+echo "###!!! Hello"
