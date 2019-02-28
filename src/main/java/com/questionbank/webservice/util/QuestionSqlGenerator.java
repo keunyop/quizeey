e848e9421e2a1232da441ca32ff05d8839aba1d6
keunyop <kylee1112@hotmail.com>
2019-02-28 12:41:54 +0900

    gen sql

--- src/main/java/com/questionbank/webservice/util/QuestionSqlGenerator.java
+++ src/main/java/com/questionbank/webservice/util/QuestionSqlGenerator.java
@@ -3,9 +3,12 @@ package com.questionbank.webservice.util;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class QuestionSqlGenerator {
 
@@ -15,30 +18,30 @@ public class QuestionSqlGenerator {
     final String VER_NBR   = "2";
     final String VER_NAME  = "AWS-Certified-Developer-Associate V13.25";
 
-    //    public static void main(String[] args) {
-    //        QuestionSqlGenerator qsGen = new QuestionSqlGenerator();
-    //
-    //        StringBuilder sb = new StringBuilder();
-    //        sb.append("-- TEST\n");
-    //        sb.append(qsGen._genInsertTestSql());
-    //        sb.append("\n-- VERSION\n");
-    //        sb.append(qsGen._genInsertVersionSql());
-    //
-    //        int qNum = 0;
-    //        for (String line : qsGen._readFile().collect(Collectors.toList())) {
-    //            qNum++;
-    //            sb.append("\n-- Q" + qNum + "\n");
-    //            sb.append(qsGen._genInsertQuestionSql(line));
-    //            sb.append("\n");
-    //            sb.append(qsGen._genInsertExampleSql(line));
-    //            sb.append("\n");
-    //
-    //            break;
-    //        }
-    //
-    //        System.out.println(sb.toString());
-    //
-    //    }
+    public static void main(String[] args) {
+        QuestionSqlGenerator qsGen = new QuestionSqlGenerator();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("-- TEST\n");
+        sb.append(qsGen._genInsertTestSql());
+        sb.append("\n-- VERSION\n");
+        sb.append(qsGen._genInsertVersionSql());
+
+        int qNum = 0;
+        for (String line : qsGen._readFile().collect(Collectors.toList())) {
+            qNum++;
+            sb.append("\n-- Q" + qNum + "\n");
+            sb.append(qsGen._genInsertQuestionSql(line));
+            sb.append("\n");
+            sb.append(qsGen._genInsertExampleSql(line));
+            sb.append("\n");
+
+            break;
+        }
+
+        System.out.println(sb.toString());
+
+    }
 
     private String _genInsertTestSql() {
         return String.format("insert into test (test_nm, created_date, modified_date) values ('%s', now(), now());",
@@ -64,7 +67,7 @@ public class QuestionSqlGenerator {
                 TEST_ID, VER_NBR, questNbr, questTxt, explanation, reference);
     }
 
-    private String _genInsertExampleSql(String line) {
+    private StringBuilder _genInsertExampleSql(String line) {
         String[] strs = line.split("\\|\\|");
 
         String questNbr = (strs.length > 0) ? strs[0].substring(3) : "";
@@ -73,44 +76,39 @@ public class QuestionSqlGenerator {
 
         StringBuilder sb = new StringBuilder();
 
-        //        for (String example : examples) {
-        //
-        //        }
-
-        return String.format(
-                "insert into example (test_id, ver_nbr, quest_nbr, exmp_nbr, exmp_txt, answer, created_date, modified_date) values ('%s', '%s', '%s', '%s', '%s', '%s', now(), now());",
-                TEST_ID, VER_NBR, questNbr, "1", exmpTxt, answer);
-    }
-
-    private static List<String> _getExamples(String exmpTxt) {
-        int i = 1;
-        int begin, end;
-
-        List<String> examples = new ArrayList<>();
-
-        while (exmpTxt.contains(String.valueOf((char) (65 + i)) + ".")) {
-            begin = exmpTxt.indexOf(String.valueOf((char) (64 + i)) + ".");
-            end = (exmpTxt.contains(String.valueOf((char) (65 + i)) + "."))
-                    ? exmpTxt.indexOf(String.valueOf((char) (65 + i)) + ".")
-                    : exmpTxt.length();
-            i++;
-
-            examples.add(exmpTxt.substring(begin, end));
+        int exmpNbr = 1;
+        for (String example : _getExamples(exmpTxt)) {
+            sb.append(String.format(
+                    "insert into example (test_id, ver_nbr, quest_nbr, exmp_nbr, exmp_txt, answer, created_date, modified_date) values ('%s', '%s', '%s', '%s', '%s', '%s', now(), now());",
+                    TEST_ID, VER_NBR, questNbr, exmpNbr, example, ""));
+            sb.append("\n");
+            exmpNbr++;
         }
 
-        examples.add(exmpTxt.substring(exmpTxt.indexOf(String.valueOf((char) (64 + i)) + ".")));
-
-        for (String string : examples) {
-            System.out.println(string);
-        }
-
-        return null;
+        return sb;
     }
 
-    public static void main(String[] args) {
-        _getExamples(
-                "A. Yes, provided that you have root access. B. Yes, when you create a new CloudFormation template C. Yes, but not for all Regions. D. No, you can add the ReadReplica only when the resource is made available by CloudFormation. E.asdfasd F.asdfadsfasdfasdlkjlkj123123");
+    private List<String> _getExamples(String exmpTxt) {
+        return Pattern.compile("[A-Z][.]").splitAsStream(exmpTxt).filter(example -> !StringUtils.isEmpty(example))
+                .map(example -> example.trim()).collect(Collectors.toList());
     }
+
+    private boolean _isAnswer(String answerTxt, int exmpNbr) {
+        char answerChar = answerTxt.replace("Answer:", "").trim().charAt(0);
+        return Character.getNumericValue(answerChar) - 9 == exmpNbr;
+    }
+
+    //    public static void main(String[] args) {
+    //        //            List<String> strList = _getExamples(
+    //        //                    "A. Yes, provided that you have root access. B. Yes, when you create a new CloudFormation template C. Yes, but not for all Regions. D. No, you can add the ReadReplica only when the resource is made available by CloudFormation. E.asdfasd F.asdfadsfasdfasdlkjlkj123123");
+    //        //    
+    //        //            for (String string : strList) {
+    //        //                System.out.println(string);
+    //        //            }
+    //
+    //        System.out.println(_isAnswer("Answer: B  ", 2));
+    //
+    //    }
 
     private Stream<String> _readFile() {
         Stream<String> stream = null;
