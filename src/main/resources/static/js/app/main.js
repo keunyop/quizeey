70a01596354d959894a4166c015fa08f1c9a332c
keunyop <kylee1112@hotmail.com>
2019-03-03 06:22:54 +0900

    bug fix

--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -23,19 +23,6 @@ var main = {
     openQuiz : function (testId) {
         window.location = '/test?testId=' + testId;
     }
-    // ,
-    // disqus : function () {
-    //     var disqus_config = function () {
-    //         this.page.url = 'quizeey.com';  // Replace PAGE_URL with your page's canonical URL variable
-    //         this.page.identifier = 'main'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    //     };
-    //     (function() { // DON'T EDIT BELOW THIS LINE
-    //         var d = document, s = d.createElement('script');
-    //         s.src = 'https://testasdf-1.disqus.com/embed.js';
-    //         s.setAttribute('data-timestamp', +new Date());
-    //         (d.head || d.body).appendChild(s);
-    //     })();
-    // }
 };
 
 main.init();
