d02e77c1ba6da356a00469f5ce556078ab773e93
keunyop <kylee1112@hotmail.com>
2019-03-07 14:39:47 +0900

    request form done

--- src/main/resources/static/js/app/test.js
+++ src/main/resources/static/js/app/test.js
@@ -178,19 +178,6 @@ var test;
             }).fail(function (error) {
                 alert(error);
             });
-
-            // var disqus = function(pageIdentifier) {
-            //     var disqus_config = function () {
-            //         // this.page.url = 'quizeey.com';  // Replace PAGE_URL with your page's canonical URL variable
-            //         this.page.identifier = 'question-' + pageIdentifier; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-            //     };
-            //     (function() { // DON'T EDIT BELOW THIS LINE
-            //         var d = document, s = d.createElement('script');
-            //         s.src = 'https://testasdf-1.disqus.com/embed.js';
-            //         s.setAttribute('data-timestamp', +new Date());
-            //         (d.head || d.body).appendChild(s);
-            //     })();
-            // };
         }
     };
 })();
