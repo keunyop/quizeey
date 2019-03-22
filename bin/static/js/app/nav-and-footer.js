849e38f27326734f89a79b34aae6228ac2f03349
keunyop <kylee1112@hotmail.com>
2019-03-23 08:11:07 +0900

    Korean page developing

--- bin/static/js/app/nav-and-footer.js
+++ bin/static/js/app/nav-and-footer.js
@@ -1,4 +1,11 @@
 $(function(){
-	$("#nav-placeholder").load("/html/nav.html");
-	$("#footer-placeholder").load("/html/footer.html");
+	var language = $("meta[http-equiv='content-language']").attr("content")
+
+	if (language == 'ko') {
+		$("#nav-placeholder").load("/html/kr/nav.html");
+		$("#footer-placeholder").load("/html/kr/footer.html");
+	} else {
+		$("#nav-placeholder").load("/html/nav.html");
+		$("#footer-placeholder").load("/html/footer.html");
+	}
 });
