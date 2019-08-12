97444d4b73e8b9b9590fbd3c1b6aef9c69dd16fe
keunyop <kylee1112@hotmail.com>
2019-08-13 07:07:50 +0900

    search

--- bin/static/js/app/main.js
+++ bin/static/js/app/main.js
@@ -1,58 +1,73 @@
-var main_div =  $("#main-div");
+var main_div = $("#main-div");
 
 var main = {
-    init : function () {
-        var _this = this;
-        var $btn_take_the_quiz = $('#btn-take-the-quiz');
+  init: function() {
+    var _this = this;
+    var $btn_take_the_quiz = $("#btn-take-the-quiz");
 
-        $btn_take_the_quiz.on('click', function () {
-            _this.openQuiz();
-        });
+    $btn_take_the_quiz.on("click", function() {
+      _this.openQuiz();
+    });
 
-        // Radio button change event123
-        $('input:radio[name="testRadio"]').on('change', function() {
-            _this.takeTheQuizBtnChange($btn_take_the_quiz);
-        });
+    // Radio button change event123
+    $('input:radio[name="testRadio"]').on("change", function() {
+      _this.takeTheQuizBtnChange($btn_take_the_quiz);
+    });
 
-        // Keyboard press event
-        $(document).keypress(function(e) {
-	        // 키보드 숫자 1~9
-            // keyCode 49~57
-            if (e.keyCode >= 49 && e.keyCode <= 57) {
-                var radioNbr = e.keyCode-48;
-                $("#testRadio" + radioNbr).prop("checked", true);
+    // Keyboard press event
+    $(document).keypress(function(e) {
+      // 키보드 숫자 1~9
+      // keyCode 49~57
+      if (e.keyCode >= 49 && e.keyCode <= 57) {
+        var radioNbr = e.keyCode - 48;
+        $("#testRadio" + radioNbr).prop("checked", true);
 
-                _this.takeTheQuizBtnChange($btn_take_the_quiz);
-            }
-            // 엔터키
-            else if (e.keyCode == 13) {
-                _this.openQuiz();
-            }
-        });
-    },
-    openQuiz : function () {
-        var path = '/';
-        var testNm = $("input[name='testRadio']:checked").val();
+        _this.takeTheQuizBtnChange($btn_take_the_quiz);
+      }
+      // 엔터키
+      else if (e.keyCode == 13) {
+        _this.openQuiz();
+      }
+    });
 
-        if (!testNm) {
-            return;
-        }
+    $("#testSearch").on("keyup", function() {
+      var value = $(this)
+        .val()
+        .toLowerCase();
+      $("#testRadios *").filter(function() {
+        $(this).toggle(
+          $(this)
+            .text()
+            .toLowerCase()
+            .indexOf(value) > -1
+        );
+      });
+    });
+  },
 
-        // Language
-        if ($("meta[http-equiv='content-language']").attr("content") == 'en') {
-            path = '/en/';
-        }
+  openQuiz: function() {
+    var path = "/";
+    var testNm = $("input[name='testRadio']:checked").val();
 
-        window.location = path + testNm;
-    },
-
-    // Radio button change event
-    takeTheQuizBtnChange : function ($btn_take_the_quiz) {
-        if ($btn_take_the_quiz.prop("disabled")) {
-            $btn_take_the_quiz.removeClass('btn-secondary').addClass('btn-primary');
-            $btn_take_the_quiz.prop("disabled", false);          
-        }      
+    if (!testNm) {
+      return;
     }
+
+    // Language
+    if ($("meta[http-equiv='content-language']").attr("content") == "en") {
+      path = "/en/";
+    }
+
+    window.location = path + testNm;
+  },
+
+  // Radio button change event
+  takeTheQuizBtnChange: function($btn_take_the_quiz) {
+    if ($btn_take_the_quiz.prop("disabled")) {
+      $btn_take_the_quiz.removeClass("btn-secondary").addClass("btn-primary");
+      $btn_take_the_quiz.prop("disabled", false);
+    }
+  }
 };
 
 main.init();
