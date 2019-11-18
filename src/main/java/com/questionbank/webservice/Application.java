e83c78abfd117906bbc1a3ada3ad2f36ac2048c8
keunyop <kylee1112@hotmail.com>
2019-11-18 23:44:04 +0900

    설정변경

--- src/main/java/com/questionbank/webservice/Application.java
+++ src/main/java/com/questionbank/webservice/Application.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location=" + "classpath:application.yml,"
-            + "/app/config/real-application.yml";
+            + "/home/ec2-user/app/config/real-application.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).properties(APPLICATION_LOCATIONS).run(args);
