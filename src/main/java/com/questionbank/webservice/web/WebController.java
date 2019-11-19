2a27e4c2ec29f65e91f7ee0039f929e0d2ac17cf
keunyop <kylee1112@hotmail.com>
2019-11-20 00:18:20 +0900

    위험물

--- src/main/java/com/questionbank/webservice/web/WebController.java
+++ src/main/java/com/questionbank/webservice/web/WebController.java
@@ -44,6 +44,7 @@ public class WebController {
             TESTMAP.put("/engineer-fire-protection-system-electrical", new String[] { "31", "소방설비기사(전기분야)" });
             TESTMAP.put("/engineer-fire-protection-system-mechanical", new String[] { "33", "소방설비기사(기계분야)" });
             TESTMAP.put("/craftsman-information-processing", new String[] { "35", "정보처리기능사" });
+            TESTMAP.put("/industrial-engineer-hazardous-material", new String[] { "37", "위험물산업기사" });
         }
     }
 
@@ -56,7 +57,7 @@ public class WebController {
             "/craftsman-cook-korean-food", "/word-processor", "/hairdresser", "/craftsman-electricity",
             "/engineer-electricity", "/engineer-industrial-safety", "/industrial-engineer-electricity",
             "/engineer-fire-protection-system-electrical", "/engineer-fire-protection-system-mechanical",
-            "/craftsman-information-processing" })
+            "/craftsman-information-processing", "/industrial-engineer-hazardous-material" })
     public String craftsmanForkLiftTruckOperator(HttpServletRequest request, Model model,
                                                  @RequestParam(value = "questId", required = false)
                                                  String questId) {
