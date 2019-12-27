ca8d8b8e62fe8c4ee885f61818baea697a64abcd
keunyop <kylee1112@hotmail.com>
2019-12-28 00:36:24 +0900

    Questions

--- src/main/java/com/questionbank/webservice/web/WebController.java
+++ src/main/java/com/questionbank/webservice/web/WebController.java
@@ -33,21 +33,21 @@ public class WebController {
             TESTMAP.put("/computer-specialist-in-spreadsheet-and-database-level-2", new String[] { "11" });
             TESTMAP.put("/computer-specialist-in-spreadsheet-and-database-level-1", new String[] { "12" });
             TESTMAP.put("/craftsman-fork-lift-truck-operator", new String[] { "13" });
-            TESTMAP.put("/craftsman-cook-korean-food", new String[] { "14", "한식조리기능사" });
-            TESTMAP.put("/word-processor", new String[] { "15", "워드프로세서" });
-            TESTMAP.put("/hairdresser", new String[] { "25", "미용사(일반)" });
-            TESTMAP.put("/craftsman-electricity", new String[] { "26", "전기기능사" });
+            TESTMAP.put("/craftsman-cook-korean-food", new String[] { "14" });
+            TESTMAP.put("/word-processor", new String[] { "15" });
+            TESTMAP.put("/hairdresser", new String[] { "25" });
+            TESTMAP.put("/craftsman-electricity", new String[] { "26" });
             TESTMAP.put("/engineer-electricity", new String[] { "27" });
-            TESTMAP.put("/engineer-industrial-safety", new String[] { "28", "산업안전기사" });
-            TESTMAP.put("/industrial-engineer-electricity", new String[] { "29", "전기산업기사" });
-            TESTMAP.put("/engineer-fire-protection-system-electrical", new String[] { "31", "소방설비기사(전기분야)" });
-            TESTMAP.put("/engineer-fire-protection-system-mechanical", new String[] { "33", "소방설비기사(기계분야)" });
-            TESTMAP.put("/craftsman-information-processing", new String[] { "35", "정보처리기능사" });
-            TESTMAP.put("/industrial-engineer-hazardous-material", new String[] { "37", "위험물산업기사" });
-            TESTMAP.put("/engineer-general-machinery", new String[] { "40", "일반기계기사" });
-            TESTMAP.put("/industrial-engineer-information-processing", new String[] { "41", "정보처리산업기사" });
-            TESTMAP.put("/industrial-engineer-office-automation", new String[] { "42", "사무자동화산업기사" });
-            TESTMAP.put("/craftsman-hazardous-material", new String[] { "44", "위험물기능사" });
+            TESTMAP.put("/engineer-industrial-safety", new String[] { "28" });
+            TESTMAP.put("/industrial-engineer-electricity", new String[] { "29" });
+            TESTMAP.put("/engineer-fire-protection-system-electrical", new String[] { "31" });
+            TESTMAP.put("/engineer-fire-protection-system-mechanical", new String[] { "33" });
+            TESTMAP.put("/craftsman-information-processing", new String[] { "35" });
+            TESTMAP.put("/industrial-engineer-hazardous-material", new String[] { "37" });
+            TESTMAP.put("/engineer-general-machinery", new String[] { "40" });
+            TESTMAP.put("/industrial-engineer-information-processing", new String[] { "41" });
+            TESTMAP.put("/industrial-engineer-office-automation", new String[] { "42" });
+            TESTMAP.put("/craftsman-hazardous-material", new String[] { "44" });
             TESTMAP.put("/craftsman-computer-graphics-operation", new String[] { "45" });
             TESTMAP.put("/nail-technician", new String[] { "51" });
             TESTMAP.put("/industrial-engineer-industrial-safety", new String[] { "52" });
@@ -58,33 +58,71 @@ public class WebController {
         }
     }
 
-    @GetMapping(value = { "/craftsman-cook-korean-food", "/word-processor", "/hairdresser", "/craftsman-electricity",
+    @GetMapping(value = { "/engineer-information-processing", "/kr/engineer-information-processing",
+            "/craftsman-computer-graphics-operation", "/engineer-electricity", "/nail-technician",
+            "/industrial-engineer-industrial-safety", "/sqlp-professional", "/kr/sqlp-professional",
+            "craftsman-web-design", "/computer-specialist-in-spreadsheet-and-database-level-2",
+            "/en/aws-certified-developer-associate", "/cbp-basic-developer", "/kr/cbp-basic-developer",
+            "/sqlp-developer", "/kr/sqlp-developer", "/engineer-transportation", "/engineer-architecture",
+            "/engineer-broadcasting-communication", "/engineer-urban-planning", "/engineer-information-security",
+            "/esthetician", "/computer-specialist-in-spreadsheet-and-database-level-1",
+            "/craftsman-fork-lift-truck-operator", "/engineer-air-conditioning-refrigerating-machinery",
+            "/craftsman-cook-korean-food", "/word-processor", "/hairdresser", "/craftsman-electricity",
             "/engineer-industrial-safety", "/industrial-engineer-electricity",
             "/engineer-fire-protection-system-electrical", "/engineer-fire-protection-system-mechanical",
             "/craftsman-information-processing", "/industrial-engineer-hazardous-material",
             "/engineer-general-machinery", "/industrial-engineer-information-processing",
             "/industrial-engineer-office-automation", "/craftsman-hazardous-material" })
-    public String craftsmanForkLiftTruckOperator(HttpServletRequest request, Model model,
-                                                 @RequestParam(value = "questId", required = false)
-                                                 String questId) {
+    public String question(HttpServletRequest request, Model model, @RequestParam(value = "questId", required = false)
+    String questId) {
 
         String uri = request.getServletPath();
+        String prefix = "test";
 
         if (uri.startsWith("/kr/")) {
             uri = uri.substring(3);
+        } else if (uri.startsWith("/en/")) {
+            uri = uri.substring(3);
+            prefix = "en/test";
         }
 
-        String[] testInfos = TESTMAP.get(uri);
-
-        model.addAttribute("testId", testInfos[0]);
-        model.addAttribute("testName", testInfos[1]);
+        model.addAttribute("testId", TESTMAP.get(uri)[0]);
 
         if (StringUtils.isNotBlank(questId)) {
             model.addAttribute("inputQuestId", questId);
         }
 
-        return "test";
+        return prefix + uri;
+
     }
+
+    //    @GetMapping(value = { "/craftsman-cook-korean-food", "/word-processor", "/hairdresser", "/craftsman-electricity",
+    //            "/engineer-industrial-safety", "/industrial-engineer-electricity",
+    //            "/engineer-fire-protection-system-electrical", "/engineer-fire-protection-system-mechanical",
+    //            "/craftsman-information-processing", "/industrial-engineer-hazardous-material",
+    //            "/engineer-general-machinery", "/industrial-engineer-information-processing",
+    //            "/industrial-engineer-office-automation", "/craftsman-hazardous-material" })
+    //    public String craftsmanForkLiftTruckOperator(HttpServletRequest request, Model model,
+    //                                                 @RequestParam(value = "questId", required = false)
+    //                                                 String questId) {
+    //
+    //        String uri = request.getServletPath();
+    //
+    //        if (uri.startsWith("/kr/")) {
+    //            uri = uri.substring(3);
+    //        }
+    //
+    //        String[] testInfos = TESTMAP.get(uri);
+    //
+    //        model.addAttribute("testId", testInfos[0]);
+    //        model.addAttribute("testName", testInfos[1]);
+    //
+    //        if (StringUtils.isNotBlank(questId)) {
+    //            model.addAttribute("inputQuestId", questId);
+    //        }
+    //
+    //        return "test";
+    //    }
 
     @GetMapping(value = { "/", "/kr", "/en" })
     public String main(HttpServletRequest request) {
@@ -210,35 +248,4 @@ public class WebController {
         return "privacy";
     }
 
-    @GetMapping(value = { "/engineer-information-processing", "/kr/engineer-information-processing",
-            "/craftsman-computer-graphics-operation", "/engineer-electricity", "/nail-technician",
-            "/industrial-engineer-industrial-safety", "/sqlp-professional", "/kr/sqlp-professional",
-            "craftsman-web-design", "/computer-specialist-in-spreadsheet-and-database-level-2",
-            "/en/aws-certified-developer-associate", "/cbp-basic-developer", "/kr/cbp-basic-developer",
-            "/sqlp-developer", "/kr/sqlp-developer", "/engineer-transportation", "/engineer-architecture",
-            "/engineer-broadcasting-communication", "/engineer-urban-planning", "/engineer-information-security",
-            "/esthetician", "/computer-specialist-in-spreadsheet-and-database-level-1",
-            "/craftsman-fork-lift-truck-operator", "/engineer-air-conditioning-refrigerating-machinery" })
-    public String sample(HttpServletRequest request, Model model, @RequestParam(value = "questId", required = false)
-    String questId) {
-
-        String uri = request.getServletPath();
-        String prefix = "test";
-
-        if (uri.startsWith("/kr/")) {
-            uri = uri.substring(3);
-        } else if (uri.startsWith("/en/")) {
-            uri = uri.substring(3);
-            prefix = "en/test";
-        }
-
-        model.addAttribute("testId", TESTMAP.get(uri)[0]);
-
-        if (StringUtils.isNotBlank(questId)) {
-            model.addAttribute("inputQuestId", questId);
-        }
-
-        return prefix + uri;
-
-    }
 }
