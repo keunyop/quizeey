39df0a274ed33321c7d042daaf2a34feee9a2bac
keunyop <kylee1112@hotmail.com>
2020-01-15 00:37:06 +0900

    add test

--- src/main/java/com/questionbank/webservice/web/WebController.java
+++ src/main/java/com/questionbank/webservice/web/WebController.java
@@ -94,6 +94,7 @@ public class WebController {
             TESTMAP.put("/linux-master-2", new String[] { "110" });
             TESTMAP.put("/1st-grade-social-worker-1", new String[] { "111" });
             TESTMAP.put("/computer-accounting-1", new String[] { "112" });
+            TESTMAP.put("/1st-grade-social-worker-2", new String[] { "113" });
         }
     }
 
@@ -125,7 +126,7 @@ public class WebController {
             "/engineer-gas", "/craftsman-elevator", "/craftsman-air-conditioning-and-refrigerating-machinery",
             "/craftsman-environmental", "/engineer-food-processing", "/industrial-engineer-gas",
             "/clinical-psychology-practitioner", "/nursing-assistant", "/linux-master-2", "/1st-grade-social-worker-1",
-            "/computer-accounting-1" })
+            "/computer-accounting-1", "/1st-grade-social-worker-2" })
     public String question(HttpServletRequest request, Model model, @RequestParam(value = "questId", required = false)
     String questId) {
 
