acfa97e81edc45ae9b5e8f7b2cddd0e0eea73a53
keunyop <kylee1112@hotmail.com>
2020-03-27 16:55:18 +0900

    add test

--- src/main/java/com/questionbank/webservice/web/WebController.java
+++ src/main/java/com/questionbank/webservice/web/WebController.java
@@ -167,6 +167,7 @@ public class WebController {
             TESTMAP.put("/az-301", new String[] { "187" });
             TESTMAP.put("/engineer-elevator", new String[] { "188" });
             TESTMAP.put("/industrial-engineer-visual-communication-design", new String[] { "189" });
+            TESTMAP.put("/ms-100", new String[] { "190" });
         }
     }
 
@@ -224,7 +225,7 @@ public class WebController {
             "/ccnp-300-101", "/en/ccnp-300-101", "/comptia-220-1001", "/en/comptia-220-1001", "/comptia-n10-007",
             "/en/comptia-n10-007", "/microsoft-70-740", "/en/microsoft-70-740", "/engineer-plant-maintenance",
             "/pmi-pmp", "/en/pmi-pmp", "/craftsman-chemical-analysis", "/az-301", "/en/az-301", "/engineer-elevator",
-            "/industrial-engineer-visual-communication-design" })
+            "/industrial-engineer-visual-communication-design", "/ms-100", "/en/ms-100" })
     public String question(HttpServletRequest request, Model model, @RequestParam(value = "questId", required = false)
     String questId) {
 
