04bdaacf74d88e88c0534985e87f68c8f3efe716
keunyop <kylee1112@hotmail.com>
2020-10-15 13:08:00 +0900

    develpoing

--- src/main/java/com/questionbank/webservice/web/WebController.java
+++ src/main/java/com/questionbank/webservice/web/WebController.java
@@ -192,6 +192,7 @@ public class WebController {
 	    TESTMAP.put("/sport-business-manager", new String[] { "217" });
 	    TESTMAP.put("/industrial-engineer-energy-management", new String[] { "218" });
 	    TESTMAP.put("/engineer-construction-equipment", new String[] { "219" });
+	    TESTMAP.put("/industrial-engineer-quality-management", new String[] { "220" });
 	}
     }
 
@@ -257,7 +258,7 @@ public class WebController {
 	    "/craftsman-mushroom-seeds", "/craftsman-production-automatic-operation", "/engineer-colorist",
 	    "/industrial-engineer-interior-architecture", "/craftsman-seeds", "/engineer-seeds", "/craftsman-computer",
 	    "/engineer-cadastral-surveying", "/sport-business-manager", "/industrial-engineer-energy-management",
-	    "/engineer-construction-equipment" })
+	    "/engineer-construction-equipment", "/industrial-engineer-quality-management" })
     public String question(HttpServletRequest request, Model model,
 	    @RequestParam(value = "questId", required = false) String questId) {
 
